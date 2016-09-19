21ac52354817299882807c0d6a3c9c4cfb372130
nicolaspoulain <nico.poulain@gmail.com>
2016-09-19 14:31:46 +0200

    No page caching on module

--- modules/custom/bb/src/Controller/ModuleNG.php
+++ modules/custom/bb/src/Controller/ModuleNG.php
@@ -24,11 +24,14 @@ class ModuleNG extends ControllerBase {
     $content['intro'] = [ '#markup' => 'src/Controller/ModuleNG.php' ];
     // See bb.module function bbtheme
     $content['#theme'] = 'moduleng';
-    // moduleng-layout/html.twig 
+    // moduleng-layout/html.twig
 
     // Get form
     $content['sessions'] =
       \Drupal::formBuilder()->getForm('Drupal\bb\Form\SessionsTableForm');
+
+    // Disable page caching on the current request.
+    \Drupal::service('page_cache_kill_switch')->trigger();
 
     // dpm($content);
     return $content;
