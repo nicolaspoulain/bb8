cbc938176cbbf63377729a3a4b27cab318778d50
nicolaspoulain <nico.poulain@gmail.com>
2016-11-04 10:51:06 +0100

    cfile

---
+++ modules/custom/bb/src/Form/CfileForm.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\bb\Form\CfileForm.
+ */
+
+namespace Drupal\bb\Form;
+
+use Drupal\Core\Ajax\ChangedCommand;
+use Drupal\Core\Ajax\InvokeCommand;
+
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CssCommand;
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\File\Entity\File;
+use Drupal\bb\Controller\BbCrudController;
+use Drupal\Core\Config\ConfigFactory;
+
+/**
+ * Implements the CfileForm form controller.
+ */
+class CfileForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return 'Fichiers pour les administratifs';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'CfileForm';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    // Ajouter un fichier
+    $form['afile'] = array(
+      '#title' => t('Fichiers entre conseillers'),
+      '#type' => 'managed_file',
+      '#upload_validators'  => array(
+        'file_validate_extensions' => array('gif png jpg jpeg'),
+        'file_validate_size' => array(25600000),
+      ),
+      // '#upload_location' => 'public://images/',
+      '#upload_location' => 'private://images/',
+      '#required' => FALSE,
+      // '#element_validate' => array( array($this, 'saveCfile'), ), // callback
+    );
+    $form['submit_file'] = array(
+      '#type' => 'submit',
+      '#value' => t('Submit'),
+    );
+
+    // Supprimer un fichier
+    $files = BbCrudController::load( 'gbb_file', ['co_modu' => '38967', 'co_degre' => '2', 'zone' => 2]);
+    foreach ($files as $f) {
+      $file_loaded = BbCrudController::load( 'file_managed', ['fid' => $f->fid]);
+      // dpm($file_loaded);
+      $flist[$f->fid] = $file_loaded[0]->filename;
+    }
+    $form['fileToDelete'] = array(
+      '#type'    => 'radios',
+      '#options' => $flist,
+    );
+    $form['delete_file'] = array(
+      '#type' => 'submit',
+      '#value' => t('Delete'),
+      '#submit' => array('::deleteForm'),
+    );
+    // $form['delete_file']['#submit'][] = 'delete_form';
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /* Fetch the array of the file stored temporarily in database */
+    $afile = $form_state->getValue('afile');
+    /* Load the object of the file by it's fid */
+    dpm($afile[0]);
+    $file = File::load( $afile[0] );
+    /* Set the status flag permanent of the file object */
+    $file->setPermanent();
+    /* Save the file in database */
+    $file->save();
+    $current_uri = \Drupal::request()->getRequestUri();
+    $path_args = array_slice(explode('/',$current_uri),-2,2);
+    $entry = array(
+      'co_degre' => $path_args[0],
+      'co_modu'  => explode('?',$path_args[1])[0],
+      'fid' => $afile[0],
+      'zone' => 2,
+    );
+    // dpm($entry);
+    $module = BbCrudController::create( 'gbb_file', $entry);
+  }
+  public function deleteForm(array &$form, FormStateInterface $form_state) {
+    /* Fetch the array of the file stored temporarily in database */
+    $afile = $form_state->getValue('fileToDelete');
+
+    //  *************************
+    // POURQUOI CA MARCHE PAS ????
+    //  *************************
+    /* Load the object of the file by it's fid */
+    // $file = File::load( $afile );
+    /* Set the status flag temporary of the file object */
+    // $file->setTemporary();
+    /* Save the file in database */
+    // $file->save();
+    $current_uri = \Drupal::request()->getRequestUri();
+    $path_args = array_slice(explode('/',$current_uri),-2,2);
+    $entry = array(
+      'fid' => $afile,
+    );
+    dpm($afile);
+    $module = BbCrudController::delete( 'gbb_file', $entry);
+  }
+}
+
