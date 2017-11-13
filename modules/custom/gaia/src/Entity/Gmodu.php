a8b318cfbf5d93d411e5f777a7c48a581edb4c0c
nicolaspoulain <nico.poulain@gmail.com>
2017-11-13 16:15:06 +0100

    Liste des modules par conseiller refactor

--- modules/custom/gaia/src/Entity/Gmodu.php
+++ modules/custom/gaia/src/Entity/Gmodu.php
@@ -67,6 +67,16 @@ class Gmodu extends ContentEntityBase implements ContentEntityInterface {
       ->setDescription(t('Code du module'))
       ->setReadOnly(TRUE);
 
+    $fields['co_anmo'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('co_anmo'))
+      ->setDescription(t('Code annulation du module'))
+      ->setReadOnly(TRUE);
+
+    $fields['co_degre'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('co_degre'))
+      ->setDescription(t('Degré du module'))
+      ->setReadOnly(TRUE);
+
     $fields['co_type'] = BaseFieldDefinition::create('string')
       ->setLabel(t('co_type'))
       ->setDescription(t('Type du module'))
@@ -76,6 +86,97 @@ class Gmodu extends ContentEntityBase implements ContentEntityInterface {
       ->setLabel(t('lib'))
       ->setDescription(t('Libelle court du module'))
       ->setReadOnly(TRUE);
+
+    $fields['lcont'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('lcont'))
+      ->setDescription(t('Contenu littéral du module'))
+      ->setReadOnly(TRUE);
+
+    $fields['lpeda'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('lpeda'))
+      ->setDescription(t('Objectif pédagogique du module'))
+      ->setReadOnly(TRUE);
+
+    $fields['lmoda'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('lmoda'))
+      ->setDescription(t('Modalité du module'))
+      ->setReadOnly(TRUE);
+
+    $fields['lform'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('lform'))
+      ->setDescription(t('Forme du module'))
+      ->setReadOnly(TRUE);
+
+    $fields['lcibl'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('lcibl'))
+      ->setDescription(t('Public cible'))
+      ->setReadOnly(TRUE);
+
+    $fields['dt_crea'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('dt_crea'))
+      ->setDescription(t('Date création'))
+      ->setReadOnly(TRUE);
+
+    $fields['co_cibl'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('co_cibl'))
+      ->setDescription(t('Code du public cible'))
+      ->setReadOnly(TRUE);
+
+    $fields['co_etab_dem'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('co_etab_dem'))
+      ->setDescription(t('RNE de l etablissement demandeur'))
+      ->setReadOnly(TRUE);
+
+    $fields['co_prna'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('co_prna'))
+      ->setDescription(t('Code Priorite national'))
+      ->setReadOnly(TRUE);
+
+    $fields['co_prac'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('co_prac'))
+      ->setDescription(t('Code Priorite academique'))
+      ->setReadOnly(TRUE);
+
+    $fields['cout_p_prest'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('cout_p_prest'))
+      ->setDescription(t('Coût prévisionnel de prestation par groupe'))
+      ->setReadOnly(TRUE);
+
+    $fields['cout_p_fonc'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('cout_p_fonc'))
+      ->setDescription(t('Coût prévisionnel de fonctionnement par groupe'))
+      ->setReadOnly(TRUE);
+
+    $fields['cout_p_excep'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('cout_p_excep'))
+      ->setDescription(t('Coût prévisionnel des frais exceptionnels'))
+      ->setReadOnly(TRUE);
+
+    $fields['nb_groupe'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('nb_groupe'))
+      ->setDescription(t('Nombre de groupes possibles'))
+      ->setReadOnly(TRUE);
+
+    $fields['nb_eff_groupe'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('nb_eff_groupe'))
+      ->setDescription(t('Effectif par groupe'))
+      ->setReadOnly(TRUE);
+
+    $fields['duree_prev'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('duree_prev'))
+      ->setDescription(t('Duree prevue'))
+      ->setReadOnly(TRUE);
+
+    $fields['nb_place_prev'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('nb_place_prev'))
+      ->setDescription(t('Nombre de places prévues'))
+      ->setReadOnly(TRUE);
+
+    $fields['nb_h_interv'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('nb_h_interv'))
+      ->setDescription(t('Nombre heures intervention'))
+      ->setReadOnly(TRUE);
+
 
 
     return $fields;
--- modules/custom/gaia/src/Entity/GmoduViewsData.php
+++ modules/custom/gaia/src/Entity/GmoduViewsData.php
@@ -1,4 +1,5 @@
 <?php
+# vim: foldmarker={{,}} foldlevel=0 foldmethod=marker :
 
 namespace Drupal\gaia\Entity;
 
@@ -17,6 +18,329 @@ class GmoduViewsData extends EntityViewsData {
 
     // Additional information for Views integration, such as table joins, can be
     // put here.
+
+    // ********************************************************
+    // * gbb_gmodu ** et toutes ses colonnes + sessions spéciales
+    // ********************************************************
+    $data['gbb_nprna']['table'] = array( 'group' => t('gbb_gmodu'),
+    //{{ *** GBB_NPRNA intégré
+      'join' => array(
+        'gbb_gmodu' => array(
+          'left_field' => 'co_prna',
+          'field' => 'co_prna',
+        ),
+      ),
+    );
+    $data['gbb_nprna']['lib_court'] = array( 'help' => t('Priorite nationale : libellé court'),
+    //{{
+      'title' => t('nprna.lib_court'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    $data['gbb_nprna']['lib_long'] = array( 'help' => t('Priorite nationale : libellé long'),
+    //{{
+      'title' => t('nprna.lib_long'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    //}}
+    $data['gbb_nprac']['table'] = array( 'group' => t('gbb_gmodu'),
+    //{{ *** GBB_NPRAC intégré
+      'join' => array(
+        'gbb_gmodu' => array(
+          'left_field' => 'co_prac',
+          'field' => 'co_prac',
+        ),
+      ),
+    );
+    $data['gbb_nprac']['lib_court'] = array( 'help' => t('Priorite nationale : libellé court'),
+    //{{
+      'title' => t('nprac.lib_court'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    $data['gbb_nprac']['lib_long'] = array( 'help' => t('Priorite nationale : libellé long'),
+    //{{
+      'title' => t('nprac.lib_long'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    //}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $data['gbb_gmodu_taxonomy']['table'] = array( 'group' => t('gbb_gmodu'),
+    //{{
+      'join' => array(
+        'gbb_gmodu' => array(
+          'left_field' => 'co_modu',
+          'field' => 'co_modu',
+        ),
+      ),
+    );
+    //}}
+    $data['gbb_gmodu_taxonomy']['tid'] = array( 'help' => t('taxonomy id'),
+    //{{
+      'title' => t('tid'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    $data['gbb_gmodu_taxonomy']['weight'] = array( 'help' => t('weight'),
+    //{{
+      'title' => t('weight'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    $data['taxonomy_term_field_data_2']['table'] = array( 'group' => t('gbb_gmodu'),
+    //{{
+      'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'taxonomy_term_field_data',
+          'left_table' => 'gbb_gmodu_taxonomy',
+          'left_field' => 'tid',
+          'field' => 'tid',
+        ),
+      ),
+    );
+    //}}
+    $data['taxonomy_term_field_data_2']['name'] = array( 'help' => t('Nom du terme de taxonomie'),
+    //{{
+      'title' => t('name'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    $data['taxonomy_term_field_data_2']['vid'] = array( 'help' => t('Vocabulaire de taxonomie'),
+    //{{
+      'title' => t('vid'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    $data['taxonomy_term_field_data_2']['description__value'] = array( 'help' => t('Nom abbrégé du terme de taxonomie'),
+    //{{
+      'title' => t('description__value'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    //{{ Responsable ORGA    pour un module (co_tres=2)
+    $data['gbb_gdire_orga']['table'] = array( 'group' => t('gbb_gdire_orga'),
+      'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_gdire',
+          'left_field' => 'co_modu',
+          'field' => 'co_modu',
+          'extra' => 'gbb_gdire_orga.co_degre = gbb_gmodu.co_degre AND gbb_gdire_orga.co_tres=2',
+        ),
+      ),
+    );
+    $data['gbb_gresp_orga']['table'] = array( 'group' => t('gbb_gmodu'),
+      'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_gresp',
+          'left_table' => 'gbb_gdire_orga',
+          'left_field' => 'co_resp',
+          'field' => 'co_resp',
+          'extra' => 'gbb_gresp_orga.co_degre = gbb_gmodu.co_degre',
+        ),
+      ),
+    );
+    //}}
+    $data['gbb_gresp_orga']['nomu'] = array( 'help' => t('Nom du responsable organisationnel'),
+      //{{
+      'title' => t('gresp.nomu orga'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    $data['gbb_gresp_orga']['prenom'] = array( 'help' => t('Prénom du responsable organisationnel'),
+      //{{
+      'title' => t('gresp.prenom orga'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    //{{ Responsable PEDA    pour un module (co_tres=3)
+    $data['gbb_gdire_peda']['table'] = array( 'group' => t('gdire_peda'),
+      'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_gdire',
+          'left_field' => 'co_modu',
+          'field' => 'co_modu',
+          'extra' => 'gbb_gdire_peda.co_degre = gbb_gmodu.co_degre AND gbb_gdire_peda.co_tres=3',
+        ),
+      ),
+    );
+    $data['gbb_gresp_peda']['table'] = array( 'group' => t('gbb_gmodu'),
+      'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_gresp',
+          'left_table' => 'gbb_gdire_peda',
+          'left_field' => 'co_resp',
+          'field' => 'co_resp',
+          'extra' => 'gbb_gresp_peda.co_degre = gbb_gmodu.co_degre',
+        ),
+      ),
+    );
+    //}}
+    $data['gbb_gresp_peda']['nomu'] = array( 'help' => t('Nom du responsable pédagogique'),
+      //{{
+      'title' => t('gresp.nomu peda'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+    $data['gbb_gresp_peda']['prenom'] = array( 'help' => t('Prénom du responsable pédagogique'),
+      //{{
+      'title' => t('gresp.prenom peda'),
+      'field'  => array('id' => 'standard',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'string'),
+    );
+    //}}
+
+    //{{ Spécial pour l'affichage des compteurs de sessions
+    // ........................................................
+    // session : sess_id d'une session envoyée
+    // ........................................................
+    $data['gbb_session_sent']['table'] = array( 'group' => t('gbb_gmodu'),
+      //{{
+        'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_session',
+          'left_field' => 'co_modu',
+          'field' => 'co_modu',
+          'extra' => 'gbb_session_sent.co_degre = gbb_gmodu.co_degre AND gbb_session_sent.convoc_sent=1',
+        ),
+      ),
+    );
+    //}}
+    $data['gbb_session_sent']['sess_id'] = array( 'help' => t('Identifiant de la session sent'),
+      //{{
+      'title' => t('sess_id_sent'),
+      'field'  => array('id' => 'numeric',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'numeric'),
+    );
+    //}}
+    // ........................................................
+    // session : sess_id d'une session en attente
+    // ........................................................
+    $data['gbb_session_attente']['table'] = array( 'group' => t('gbb_gmodu'),
+      //{{
+      'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_session',
+          'left_field' => 'co_modu',
+          'field' => 'co_modu',
+          'extra' => 'gbb_session_attente.co_degre = gbb_gmodu.co_degre AND gbb_session_attente.en_attente=1',
+        ),
+      ),
+    );
+    //}}
+    $data['gbb_session_attente']['sess_id'] = array( 'help' => t('Identifiant de la session attente'),
+      //{{
+      'title' => t('sess_id_attente'),
+      'field'  => array('id' => 'numeric',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'numeric'),
+    );
+    //}}
+    // ........................................................
+    // session : sess_id d'une session en cours
+    // ........................................................
+    $data['gbb_session_encours']['table'] = array( 'group' => t('gbb_gmodu'),
+    //{{
+        'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_session',
+          'left_field' => 'co_modu',
+          'field' => 'co_modu',
+          'extra' => 'gbb_session_encours.co_degre = gbb_gmodu.co_degre AND gbb_session_encours.en_attente=0 AND gbb_session_encours.convoc_sent=0',
+        ),
+      ),
+    );
+    //}}
+    $data['gbb_session_encours']['sess_id'] = array( 'help' => t('Identifiant de la session enCours'),
+      //{{
+      'title' => t('sess_id_enCours'),
+      'field'  => array('id' => 'numeric',),
+      'sort'   => array('id' => 'standard'),
+      'filter' => array('id' => 'numeric'),
+    );
+    //}}
+    //}}
+    //{{ Resp ORGA ou PEDA   filtre pour module (co_tres=2 OU 3)
+    // Pour le FILTRE de la grand liste views admin/structure/views/view/liste_modules
+      //{{
+    $data['gbb_gdire_orga_peda']['table'] = array( 'group' => t('gbb_gdire_orga_peda'),
+      'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_gdire',
+          'left_field' => 'co_modu',
+          'field' => 'co_modu',
+          'extra' => 'gbb_gdire_orga_peda.co_degre = gbb_gmodu.co_degre AND (gbb_gdire_orga_peda.co_tres=2 OR gbb_gdire_orga_peda.co_tres=3)',
+        ),
+      ),
+    );
+    //}}
+    $data['gbb_gresp_orga_peda']['table'] = array( 'group' => t('gbb_gmodu'),
+      //{{
+      'join' => array(
+        'gbb_gmodu' => array(
+          'table' => 'gbb_gresp',
+          'left_table' => 'gbb_gdire_orga_peda',
+          'left_field' => 'co_resp',
+          'field' => 'co_resp',
+          'extra' => 'gbb_gresp_orga_peda.co_degre = gbb_gmodu.co_degre',
+        ),
+      ),
+    );
+    //}}
+    $data['gbb_gresp_orga_peda']['resp_filter'] = array(
+      //{{
+      'title' => t('CUSTOM responsable orga/peda'),
+      'filter' => array(
+        'title' => t('CUSTOM responsable orga OU péda'),
+        'help' => t('Fltre les stages où toto est resp péda OU orga'),
+        'field' => 'nomu',
+        'id' => 'string'
+      ),
+    );
+    //}}
+    //}}
+    //}}
+
 
     return $data;
   }
