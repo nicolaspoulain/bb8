e7c958f451ec93d28b7795aa1a60c9c5e9c79082
nicolaspoulain <nico.poulain@gmail.com>
2018-05-05 16:35:09 +0200

    liste/1/1/username -> liste-modules?id_disp=1&co_anmo=1&resp_filter=Poulain

---
+++ web/modules/custom/bb/src/Controller/Liste.php
@@ -0,0 +1,34 @@
+<?php
+namespace Drupal\bb\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class Liste extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function page($id_disp,$co_anmo,$resp_filter) {
+    if ($resp_filter == "username") {
+    // Load the current user
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    // retrieve field data from that user
+    $resp_filter = $user->get('field_name')->value;
+    }
+    // query parameters
+    $routeparameters = array(
+      'id_disp' => $id_disp,
+      'co_anmo' => $co_anmo,
+      'resp_filter' => $resp_filter);
+    return $this->redirect('view.liste_modules.page_1',$routeparameters);
+  }
+
+}
+
