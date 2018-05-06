7f7c93336161a7fe418f844739fbebb35ce0ad99
nicolaspoulain <nico.poulain@gmail.com>
2018-05-06 23:23:27 +0200

    New module : change le theme de certaines pages

---
+++ web/modules/custom/pia/src/Theme/PiaNegotiator.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\pia\Theme\PiaNegotiator.
+ */
+
+namespace Drupal\pia\Theme;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+
+class PiaNegotiator implements ThemeNegotiatorInterface {
+
+  public function applies(RouteMatchInterface $route_match) {
+    $applies = FALSE;
+    $route_name = \Drupal::routeMatch()->getRouteName();
+    $pages = array(
+      'view.bb_pia.page_2',
+      'bb_pia_details_module',
+    );
+    if (in_array($route_name, $pages)) {
+      $applies = TRUE;
+    }
+    // Use this theme negotiator.
+    return $applies;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    return 'seven';
+  }
+}
