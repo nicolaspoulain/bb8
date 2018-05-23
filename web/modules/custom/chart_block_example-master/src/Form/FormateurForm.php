e6895afe184004ff3efa21872f7aa895a7f07673
nicolaspoulain <nico.poulain@gmail.com>
2018-05-23 16:28:19 +0200

    cleaning

--- web/modules/custom/chart_block_example-master/src/Form/FormateurForm.php
+++ web/modules/custom/chart_block_example-master/src/Form/FormateurForm.php
@@ -293,17 +293,12 @@ class FormateurForm extends FormBase {
       $entry = array(
         'val' => $form_state->getValue($type),
       );
-      // if ($form_state->getValue($type)>0) {
-        // dpm($condition);
-        // dpm($entry);
-        $row = BbCrudController::load('gbb_gresp_periodic', $condition);
-        if (!empty($row)) {
-          $DBWriteStatus = BbCrudController::update('gbb_gresp_periodic', $entry, $condition);
-        } else {
-          $DBWriteStatus = BbCrudController::create('gbb_gresp_periodic', array_merge($condition,$entry));
-          // BbCrudController::update( 'gbb_gresp_plus', $entry, $condition);
-        }
-      // }
+      $row = BbCrudController::load('gbb_gresp_periodic', $condition);
+      if (!empty($row)) {
+        $DBWriteStatus = BbCrudController::update('gbb_gresp_periodic', $entry, $condition);
+      } else {
+        $DBWriteStatus = BbCrudController::create('gbb_gresp_periodic', array_merge($condition,$entry));
+      }
     };
 
     return TRUE;
