b9e66139f2218af5430c0fe08fd946bf4a1655c3
nicolaspoulain <nico.poulain@gmail.com>
2018-09-02 15:26:08 +0200

    Planning BD

--- web/modules/custom/bb/src/Controller/Planning.php
+++ web/modules/custom/bb/src/Controller/Planning.php
@@ -15,17 +15,23 @@ class Planning extends ControllerBase {
    * @return array
    *   A simple renderable array.
    */
-  public function page($id_disp,$co_anmo,$resp_filter) {
-    if ($resp_filter == "username") {
+  public function page($id_disp,$co_anmo,$filter) {
+    $resp_filter = '';
+    $co_offreur = 'All';
+    $date = date("Y-m-d");
+    if ($filter == "username") {
       // Load the current user
       $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
       // retrieve field data from that user
       $resp_filter = $user->get('field_name')->value;
-      $date = date("Y-m-d");
+    };
+    if ($filter == "BD") {
+      $co_offreur = 1;
     };
     // query parameters
     $routeparameters = array(
       'date_ts' => $date,
+      'co_offreur' => $co_offreur,
       'resp_filter' => $resp_filter);
     return $this->redirect('view.planning.page_1',$routeparameters);
   }
