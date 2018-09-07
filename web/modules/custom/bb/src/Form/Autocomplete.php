48a20dcc84c7b8018861521d13a564a1247d52af
nicolaspoulain <nico.poulain@gmail.com>
2018-09-07 12:34:16 +0200

    Autocomplete formateur affiche decharge

--- web/modules/custom/bb/src/Form/Autocomplete.php
+++ web/modules/custom/bb/src/Form/Autocomplete.php
@@ -22,14 +22,26 @@ class Autocomplete {
     $string = $request->query->get('q');
     $matches = array();
     if ($string) {
-      $result = db_select('gbb_gresp_dafor')
-        ->fields('gbb_gresp_dafor', array('co_resp', 'nomu', 'prenom'))
+      $query = db_select('gbb_gresp_dafor','r')
         ->condition('nomu', db_like($string) . '%', 'LIKE')
-        ->range(0, 10)
-        ->execute();
+        ->fields('r', array('co_resp', 'nomu', 'prenom'))
+        ->range(0, 10);
+      $result = $query->execute();
+
+      $thisyear = date("y");
+      $thismonth = date("m");
+      $year = ($thismonth>7)?$thisyear:$thisyear-1;
+
       foreach ($result as $res) {
+        $query2 = db_select('gbb_gresp_dafor','r')
+          ->condition('r.co_resp', $res->co_resp, '=');
+        $query2->join('gbb_gresp_periodic','p',"p.co_resp=r.co_resp AND p.period=$year");
+        $query2->condition('type', db_like('dech_dafor'), 'LIKE')
+          ->fields('p', array('val'));
+        $xx = $query2->execute()->fetchAssoc();
+        $xx = ($xx['val']>0)? "**".$xx['val']."h**":"";
         $matches[] = array('value' => "$res->nomu $res->prenom (id:$res->co_resp)",
-          'label' => "$res->nomu $res->prenom (id:$res->co_resp)",) ;
+          'label' => "$res->nomu $res->prenom $xx (id:$res->co_resp)",) ;
       }
     }
     \Drupal\Core\Database\Database::setActiveConnection();
