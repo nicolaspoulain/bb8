ed08509a9ad5f85025bb338f85ccfebf75669111
nicolaspoulain <nico.poulain@gmail.com>
2019-04-10 11:36:22 +0200

    prio nat sur offres

--- web/modules/custom/offres/src/Form/PrioNatForm.php
+++ web/modules/custom/offres/src/Form/PrioNatForm.php
@@ -41,9 +41,28 @@ class PrioNatForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, $prio_nat=NULL, $co_omodu=NULL) {
 
+  $prios = array(
+0=> "-",
+1=> "Bac-3/Bac+3",
+2=>"Climscol_lutte_harcèlement",
+3=>"Devoirs_faits",
+4=>"École_maternelle",
+5=>"Éduc_artistique_et_cult",
+6=>"Éduc_valeurs_de_la_Rép",
+7=>"Élèves_à_besoins_éduc_part",
+8=>"Les_fondamentaux_(1D)",
+9=>"Les_fondamentaux_cycle_3",
+10=>"Orientation_au_collège",
+11=>"Professionnalisation_formateurs",
+12=>"Réforme_du_LGT",
+13=>"Scol_obligatoire_à_partir_de_3_ans",
+14=>"Sécurisation_des_parcours",
+15=>"Transfo_voie_professionnelle",);
+
   $form['co_omodu']  = array('#type' => 'hidden','#value' => $co_omodu );
   $form['prio_nat'] = array(
-    '#type' => 'checkbox', '#size' => 1,
+    '#type' => 'select',
+    '#options' => $prios,
     '#default_value' => (isset($prio_nat))? (int)$prio_nat : '0',
     '#ajax' => array(
       'callback' => [$this,'saveAjax'],
