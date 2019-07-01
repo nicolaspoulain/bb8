b987d10c7ee07af66a92fe1388512f909e1e3f9b
nicolaspoulain <nico.poulain@gmail.com>
2019-07-01 13:46:41 +0200

    second classement

--- web/modules/custom/pia/src/Controller/PiaController.php
+++ web/modules/custom/pia/src/Controller/PiaController.php
@@ -277,5 +277,149 @@ class PiaController extends ControllerBase {
 
     return $content;
   }
+  public function list_w($co_modu, $co_degre) {
+    $content = [];
+
+    $query = db_select('gbb_gmodu', 'm');
+    $query ->condition('m.co_modu', $co_modu, '=');
+    $query ->condition('m.co_degre', $co_degre, '=');
+    $query ->fields('m', array( 'co_modu', 'lib',));
+    foreach ($result = $query->execute()->fetchAll() as $r) {
+      $content['title'] = [
+        '#markup' => '<h1>'.$co_modu.' '.$r->lib.'</h1>',
+      ];
+    };
+    $content['message'] = [
+      '#markup' => $this->t('Multi-affichage.'),
+    ];
+
+    $headers = array(
+    '2S' => t('2S'),
+    '2S_w' => t('Web'),
+    '1P' => t('1P'),
+    '1P_w' => t('Web'),
+    'PA' => t('PA'),
+    'PA_w' => t('Web'),
+    '4E' => t('4E'),
+    '4E_w' => t('Web'),
+    );
+
+    // switch database (cf settings.php)
+    \Drupal\Core\Database\Database::setActiveConnection('external');
+    // Build query
+
+    //  -------------------------------------------------
+    //  ----- 2S ----------
+    $rows = array();
+    $query = db_select('taxonomy_term_data', 'ttd');
+    $query ->condition('ttd.vid', "56");
+    $query ->fields('ttd', array( 'name', 'tid',))
+           ->orderBy('weight', 'ASC');
+
+    // $co_degre=2;
+    // $co_modu=22222;
+    $condition = array('co_modu' => $co_modu, 'co_degre' => $co_degre);
+    $row = BbCrudController::load('gbb_gmodu_taxonomy', $condition);
+    foreach ($row as $l) {
+      if ($l->type=='w') $web[$l->tid]=$l->weight;
+    }
+    foreach ($result = $query->execute()->fetchAll() as $r) {
+      $position = $web[$r->tid];
+      $check=($position==0)? 0: 1;
+      $position = ($position==0)? 1:$position;
+      $position_w = \Drupal::formBuilder()->getForm('Drupal\pia\Form\PositionForm_w',
+        $check, $position, 'w', $r->tid, $co_modu, $co_degre);
+
+      $str = $r->name;
+
+      $rows[] = array(
+        '2S'   => array('data' => $str),
+        '2S_w' => array('data' => $position_w, 'class'=>'jaunepia'),
+      );
+    }
+
+    //  -------------------------------------------------
+    //  ----- 1P ----------
+    \Drupal\Core\Database\Database::setActiveConnection('external');
+    $query = db_select('taxonomy_term_data', 'ttd');
+    $query ->condition('ttd.vid', "55");
+    $query ->fields('ttd', array( 'name', 'tid',))
+           ->orderBy('weight', 'ASC');
+
+    $count = 0;
+    foreach ($result = $query->execute()->fetchAll() as $r) {
+      $position = $web[$r->tid];
+      $check=($position==0)? 0: 1;
+      $position = ($position==0)? 1:$position;
+      $position_w = \Drupal::formBuilder()->getForm('Drupal\pia\Form\PositionForm_w',
+        $check, $position, 'w', $r->co_orie, $co_modu, $co_degre);
+
+      $str = $r->name;
+
+      $rows[$count]['1P']   = array('data' => $str);
+      $rows[$count]['1P_w'] = array('data' => $position_w,'class'=>'jaunepia');
+      $count = $count +1;
+    }
+    //  -------------------------------------------------
+    //  ----- PA ----------
+    \Drupal\Core\Database\Database::setActiveConnection('external');
+    $query = db_select('taxonomy_term_data', 'ttd');
+    $query ->condition('ttd.vid', "57");
+    $query ->fields('ttd', array( 'name', 'tid',))
+           ->orderBy('weight', 'ASC');
+
+    $count = 0;
+    foreach ($result = $query->execute()->fetchAll() as $r) {
+      $position = $web[$r->tid];
+      $check=($position==0)? 0: 1;
+      $position = ($position==0)? 1:$position;
+      $position_w = \Drupal::formBuilder()->getForm('Drupal\pia\Form\PositionForm_w',
+        $check, $position, 'w', $r->co_orie, $co_modu, $co_degre);
+
+      $str = $r->name;
+
+      $rows[$count]['PA0']   = array('data' => $str);
+      $rows[$count]['PA0_w'] = array('data' => $position_w,'class'=>'jaunepia');
+      $count = $count +1;
+    }
+    //  -------------------------------------------------
+    //  ----- 4E ----------
+    \Drupal\Core\Database\Database::setActiveConnection('external');
+    $query = db_select('taxonomy_term_data', 'ttd');
+    $query ->condition('ttd.vid', "58");
+    $query ->fields('ttd', array( 'name', 'tid',))
+           ->orderBy('weight', 'ASC');
+
+    $count = 0;
+    foreach ($result = $query->execute()->fetchAll() as $r) {
+      $position = $web[$r->tid];
+      $check=($position==0)? 0: 1;
+      $position = ($position==0)? 1:$position;
+      $position_w = \Drupal::formBuilder()->getForm('Drupal\pia\Form\PositionForm_w',
+        $check, $position, 'w', $r->co_orie, $co_modu, $co_degre);
+
+      $str = $r->name;
+
+      $rows[$count]['4E']   = array('data' => $str);
+      $rows[$count]['4E_w'] = array('data' => $position_w,'class'=>'jaunepia');
+      $count = $count +1;
+    }
+
+    // print_r($rows);
+
+    $content['table'][] = array(
+      '#type' => 'table',
+      '#header' => $headers,
+      '#rows' => $rows,
+    );
+    $content['table'][] = array(
+      '#type' => 'pager',
+    );
+
+    // Don't cache this page.
+    $content['#cache']['max-age'] = 0;
+
+    return $content;
+  }
 }
 
---
+++ web/modules/custom/pia/src/Form/PositionForm_w.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\pia\Form\PositionForm.
+ */
+
+namespace Drupal\pia\Form;
+
+use Drupal\Core\Ajax\ChangedCommand;
+use Drupal\Core\Ajax\InvokeCommand;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CssCommand;
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\bb\Controller\BbCrudController;
+
+
+/**
+ * Implements the PositionForm form controller.
+ */
+class PositionForm_w extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return 'Modification de la position';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'PositionForm_'. $this->formId;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state,
+    $check=0, $position=1, $type='w', $tid=0, $co_modu=0, $co_degre=0) {
+
+  // $this->formId = rand(11111, 99999);
+
+  $rnd = rand(0,1000000000);
+  $form['co_modu']  = array('#type' => 'hidden','#value' => $co_modu );
+  $form['co_degre']  = array('#type' => 'hidden','#value' => $co_degre );
+  $form['tid']  = array('#type' => 'hidden','#value' => $tid );
+  // $form['co_orie']  = array('#type' => 'hidden','#value' => $co_orie );
+  $form['type']  = array('#type' => 'hidden','#value' => $type );
+  $form['rnd']  = array('#type' => 'hidden','#value' => $rnd );
+  // $form['check'.$rnd] = array(
+  $form['check'] = array(
+    '#id' => "check-$rnd",
+    '#type' => 'checkbox',
+    '#title' => '',
+    '#default_value' => $check,
+    '#prefix' => '<div class="inline">',
+    '#suffix' => '</div>',
+    '#ajax' => array(
+      'callback' => [$this,'saveAjax'],
+      'progress' => array('type' => 'throbber', 'message' => '')),
+  );
+  // $form['position'.$rnd] = array(
+  $form['position'] = array(
+    '#type' => 'textfield', '#size' => 2,
+    '#default_value' => (int)$position,
+    '#states' => array(    // This #states rule limits visibility
+      'visible' => array(  // action to take.
+        // ':input[name=check'.$rnd.']' => array('checked' => TRUE),),
+        ':input[id=check-'.$rnd.']' => array('checked' => TRUE),),
+    ),
+    '#ajax' => array(
+      'callback' => [$this,'saveAjax'],
+      'progress' => array('type' => 'throbber', 'message' => '')),
+  );
+  // Group submit handlers in an actions element with a key of "actions" so
+  // that it gets styled correctly, and so that other modules may add actions
+  // to the form.
+  // $form['actions'] = [
+    // '#type' => 'actions',
+  // ];
+
+  // Add a submit button that handles the submission of the form.
+  // $form['actions']['submit'] = [
+    // '#type'   => 'submit',
+    // '#value'  => $this->t('OK'),
+    // '#submit' => array('::submitForm'),
+    // '#states' => array(    This #states rule limits visibility
+      // 'visible' => array(  action to take.
+        // ':input[name=check'.$rnd.']' => array('checked' => TRUE),),
+    // ),
+  // ];
+  return $form;
+  }
+
+  /**
+   * Validate field.
+   */
+  protected function validate(array &$form, FormStateInterface $form_state) {
+    return TRUE;
+  }
+
+  /**
+   * Ajax callback to save field.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $valid = $this->validate($form, $form_state);
+    return TRUE;
+  }
+  /**
+   * Ajax callback to save field.
+   */
+  public function saveAjax(array &$form, FormStateInterface $form_state) {
+
+    $qu = $form_state->getUserInput('check');
+    $rnd     = $qu['rnd'];
+    $co_modu = $qu['co_modu'];
+    $co_degre= $qu['co_degre'];
+    $co_orie = 0;
+    $tid     = $qu['tid'];
+    $type    = $qu['type'];
+    $check   = $qu['check'];
+    $position= $qu['position'];
+    $entry = array(
+      'co_modu'  => $co_modu,
+      'co_degre'  => $co_degre,
+      'type'  => $type,
+      'tid'  => $tid,
+      'co_orie'  => $co_orie,
+      'weight'  => $position,
+    );
+    $condition = array(
+      'co_modu'  => $co_modu,
+      'co_degre'  => $co_degre,
+      'type'  => $type,
+      'tid'  => $tid,
+      'co_orie'  => $co_orie,
+    );
+    // dpm($entry);
+    if ($check) {
+      $DBWriteStatus = BbCrudController::delete('gbb_gmodu_taxonomy', $condition);
+      $DBWriteStatus = BbCrudController::create('gbb_gmodu_taxonomy', $entry);
+    } else {
+      $DBWriteStatus = BbCrudController::delete('gbb_gmodu_taxonomy', $condition);
+    }
+  }
+}
