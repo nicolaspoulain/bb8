1d0fe46b71daafbccab37e9374e4b2cf6b2b48ac
nicolaspoulain <nico.poulain@gmail.com>
2019-11-10 14:10:27 +0100

    intrface inspec in progress

--- web/modules/custom/bb/src/Controller/Liste.php
+++ web/modules/custom/bb/src/Controller/Liste.php
@@ -33,6 +33,24 @@ class Liste extends ControllerBase {
       'resp_filter' => $resp_filter);
     return $this->redirect('view.liste_modules.page_1',$routeparameters);
   }
+  public function page_orie($id_disp,$co_anmo,$co_orie) {
+    if ($co_orie == "co_orie") {
+      // Load the current user
+      $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+      // retrieve field data from that user
+      $co_orie = $user->get('field_orientation')->value;
+      if ($user->get('field_degre')->value == 1) {
+        $id_disp = $id_disp + 3;
+        $resp_filter = '';
+      };
+    };
+    // query parameters
+    $routeparameters = array(
+      'id_disp' => $id_disp,
+      'co_anmo' => $co_anmo,
+      'co_orie' => $co_orie);
+    return $this->redirect('view.liste_modules.page_1',$routeparameters);
+  }
 
 }
 
