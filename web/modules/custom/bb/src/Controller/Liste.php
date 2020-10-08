780c9f79abcb94c5fa3c452e03d20da88c49308a
nicolaspoulain <nico.poulain@gmail.com>
2020-10-08 10:44:52 +0200

    Degre dans list OK

--- web/modules/custom/bb/src/Controller/Liste.php
+++ web/modules/custom/bb/src/Controller/Liste.php
@@ -16,19 +16,21 @@ class Liste extends ControllerBase {
    *   A simple renderable array.
    */
   public function page($id_disp,$co_anmo,$resp_filter) {
+    // Load the current user
     if ($resp_filter == "username") {
-      // Load the current user
       $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-      // retrieve field data from that user
       $resp_filter = $user->get('field_name')->value;
+      $co_degre = 2;
       if ($user->get('field_degre')->value == 1) {
-        $id_disp = $id_disp + 3;
+        $co_degre = 1;
+        // $id_disp = $id_disp + 3;
         $resp_filter = '';
       };
     };
     // query parameters
     $routeparameters = array(
       'id_disp' => $id_disp,
+      'co_degre' => $co_degre,
       'co_anmo' => $co_anmo,
       'resp_filter' => $resp_filter);
     return $this->redirect('view.liste_modules.page_1',$routeparameters);
